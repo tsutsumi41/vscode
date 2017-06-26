3494e4c799a00177a33ea5c726e1519c5f3bdf60
t-amqi <umberos@gmail.com>
2017-06-26 16:40:02 -0700

    Add buttons to task panel

--- src/vs/workbench/parts/tasks/electron-browser/task.contribution.ts
+++ src/vs/workbench/parts/tasks/electron-browser/task.contribution.ts
@@ -93,7 +93,7 @@ import { IThemeService } from 'vs/platform/theme/common/themeService';
 let $ = Builder.$;
 let tasksCategory = nls.localize('tasksCategory', "Tasks");
 
-abstract class OpenTaskConfigurationAction extends Action {
+export abstract class OpenTaskConfigurationAction extends Action {
 
 	constructor(id: string, label: string,
 		private taskService: ITaskService,
@@ -196,7 +196,7 @@ abstract class OpenTaskConfigurationAction extends Action {
 	}
 }
 
-class ConfigureTaskRunnerAction extends OpenTaskConfigurationAction {
+export class ConfigureTaskRunnerAction extends OpenTaskConfigurationAction {
 	public static ID = 'workbench.action.tasks.configureTaskRunner';
 	public static TEXT = nls.localize('ConfigureTaskRunnerAction.label', "Configure Task Runner");
 
@@ -211,10 +211,11 @@ class ConfigureTaskRunnerAction extends OpenTaskConfigurationAction {
 		super(id, label, taskService, configurationService, editorService, fileService, contextService,
 			outputService, messageService, quickOpenService, environmentService, configurationResolverService,
 			extensionService);
+		this.class = 'task-action configure';
 	}
 }
 
-class ConfigureBuildTaskAction extends OpenTaskConfigurationAction {
+export class ConfigureBuildTaskAction extends OpenTaskConfigurationAction {
 	public static ID = 'workbench.action.tasks.configureBuildTask';
 	public static TEXT = nls.localize('ConfigureBuildTaskAction.label', "Configure Build Task");
 
